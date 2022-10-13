769e0b0472539a1f305bdf03cf82a32900545962
JuanC <juan_corica@hotmail.com>
2022-10-12 23:12:47 -0300

    finished ultimate hooks

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -18,10 +18,24 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
 
-  const create = (resource) => {
-    // ...
+    const getAll = async () => {
+      const response = await axios.get(baseUrl)
+      //console.log(response.data)
+      return setResources(response.data)
+    }
+
+    getAll()
+
+
+  },[baseUrl])
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    console.log(response.data)
+    setResources(resources.concat(response.data))
+    return response.data
   }
 
   const service = {
@@ -37,7 +51,7 @@ const App = () => {
   const content = useField('text')
   const name = useField('text')
   const number = useField('text')
-
+  
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
@@ -45,7 +59,7 @@ const App = () => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
     personService.create({ name: name.value, number: number.value})
